4b4e3f87127a2356fdf0104f0ce1671ce8fab3e9
KG LEE <acrkdduf@naver.com>
2022-11-13 22:22:40 +0900

    feat: relocation Context Provider for re-rendering optimization

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -5,20 +5,20 @@ import * as Styled from './style';
 
 function App() {
   return (
-    <ContextProvider>
-      <Styled.Container>
-        <Styled.Header>우아한 점심 룰렛</Styled.Header>
-        <Styled.GuideList>
-          <li>오늘 점심 메뉴 선택이 힘들때 간단히 룰렛을 돌려보아요!</li>
-          <li>
-            각 사무실(큰집, 작은집, 루터회관, 더큰집) 주변 식당만 대상으로 하고
-            있어요!
-          </li>
-        </Styled.GuideList>
+    <Styled.Container>
+      <Styled.Header>우아한 점심 룰렛</Styled.Header>
+      <Styled.GuideList>
+        <li>오늘 점심 메뉴 선택이 힘들때 간단히 룰렛을 돌려보아요!</li>
+        <li>
+          각 사무실(큰집, 작은집, 루터회관, 더큰집) 주변 식당만 대상으로 하고
+          있어요!
+        </li>
+      </Styled.GuideList>
+      <ContextProvider>
         <LocationSelectors />
         <Roulette />
-      </Styled.Container>
-    </ContextProvider>
+      </ContextProvider>
+    </Styled.Container>
   );
 }
 
