fe0845dcd22a3d802d53c06b508f9ea56bcf714e
KG LEE <acrkdduf@naver.com>
2022-11-15 23:46:40 +0900

    feat: sync states for Type Selectors

--- src/Context/ContextProvider.tsx
+++ src/Context/ContextProvider.tsx
@@ -7,20 +7,29 @@ import React, {
 
 const defaultValue = {
   location: '전체',
-  handleSelectLocation: (location: string) => console.log('need init'),
+  type: '전체',
+  handleSelectLocation: (location: string) => console.error('need init'),
+  handleSelectType: (type: string) => console.error('need init'),
 };
 
 export const Context = createContext(defaultValue);
 
 const ContextProvider: React.FC<PropsWithChildren> = ({ children }) => {
   const [location, setLocation] = useState(defaultValue.location);
+  const [type, setType] = useState(defaultValue.type);
 
   const handleSelectLocation = useCallback((location: string) => {
     setLocation(location);
   }, []);
 
+  const handleSelectType = useCallback((type: string) => {
+    setType(type);
+  }, []);
+
   return (
-    <Context.Provider value={{ location, handleSelectLocation }}>
+    <Context.Provider
+      value={{ location, type, handleSelectLocation, handleSelectType }}
+    >
       {children}
     </Context.Provider>
   );
