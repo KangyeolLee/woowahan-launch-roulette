ef00b86890c958df418cb5fe052a6e2652d8e632
KG LEE <acrkdduf@naver.com>
2022-11-21 01:23:28 +0900

    seperate logics for rendering & handlers apart from Component

--- src/Roulette/Roulette.tsx
+++ src/Roulette/Roulette.tsx
@@ -1,60 +1,16 @@
-import { useContext, useEffect, useRef, useState } from 'react';
-import { getRestaurants, Restaurant, RestaurantType } from '../api/restaurants';
-import { Context } from '../Context/ContextProvider';
+import { Restaurant } from '../api/restaurants';
+import useRoulette from '../hooks/useRoulette';
 import * as Styled from './style';
 
 const Roulette = () => {
-  const { location, type } = useContext(Context);
-  const [start, setStart] = useState(false);
-  const [showResult, setShowResult] = useState(false);
-  const [restaurants, setRestaurants] = useState<RestaurantType[]>([]);
-  const timerId = useRef<number>();
-
-  const filteredRestaurants = restaurants
-    .filter((restaurant) =>
-      location === '전체' ? true : restaurant.location === location
-    )
-    .filter((restaurant) =>
-      type === '전체' ? true : restaurant.type === type
-    );
-
-  const shuffle = () => {
-    const rest = restaurants.slice();
-    let len = rest.length;
-
-    while (len) {
-      const i = Math.floor(Math.random() * len--);
-      [rest[len], rest[i]] = [rest[i], rest[len]];
-    }
-
-    setRestaurants(rest);
-  };
-
-  const handleClickStart = () => {
-    !start && shuffle();
-    setStart(true);
-    timerId.current = window.setTimeout(() => setShowResult(true), 3000);
-  };
-
-  const handleClickReset = () => {
-    setStart(false);
-    setShowResult(false);
-    timerId.current && window.clearTimeout(timerId.current);
-  };
-
-  useEffect(() => {
-    (async () => {
-      const res = await getRestaurants();
-      const restaurants = res.map((r) => r.fields);
-      setRestaurants(restaurants);
-    })();
-  }, []);
-
-  useEffect(() => {
-    setStart(false);
-  }, [location, type]);
-
-  const isDisabledStartButton = filteredRestaurants.length <= 1;
+  const {
+    start,
+    showResult,
+    isDisabledStartButton,
+    filteredRestaurants,
+    handleClickStart,
+    handleClickReset,
+  } = useRoulette();
 
   return (
     <>
