85d899c39191595a03bfefca58f7008a97f54bb0
KG LEE <acrkdduf@naver.com>
2022-11-28 01:32:45 +0900

    chore: move cra files directory locatin into client directory

--- client/src/api/restaurants.ts
+++ client/src/api/restaurants.ts
@@ -18,14 +18,7 @@ export const Restaurant = Object.freeze({
 } as const);
 
 export const getRestaurants = async () => {
-  const response = await fetch(
-    `https://api.airtable.com/v0/${process.env.REACT_APP_API_RECORD_TARGET}/restaurants`,
-    {
-      headers: {
-        Authorization: `Bearer ${process.env.REACT_APP_API_KEY}`,
-      },
-    }
-  );
+  const response = await fetch(`http://localhost:5000`);
   const result = await response.json();
   return result.records as { fields: RestaurantType }[];
 };
